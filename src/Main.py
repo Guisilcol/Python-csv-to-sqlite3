cafbc661250f3385f9a630443834c2d142daf09c
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2020-12-22 16:14:44 -0300

    Update Main.py
    - Implementada configurações de ambiente prod/dev
    - Implementado menu

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,28 @@
-import controller.MainProcess as MainProcess
+import json
+import controller.FileToDatabase as FileToDatabase
+import warnings
+import Config
 
-if __name__ == "__main__":
-    welcome_message = "********** CSV to SQLITE3 **********"
+if __name__ == "__main__":    
+    config = Config.get_config_dict()
+    IS_IN_DEVELOPMENT = config['isInDevelopment']
+
+    if not IS_IN_DEVELOPMENT:
+        warnings.filterwarnings('ignore')           
+    
+    welcome_message = \
+        "       ********** CSV to SQLITE3 ********** \n\n" \
+        "1 - Exportar dados de arquivos para banco de dados \n" \
+        "2 - Transformar arquivos .csv em arquivos posicionais \n" \
+        "3 - Transformar arquivos posicionais em arquivos .csv \n"
+
     print(welcome_message)
-    MainProcess.read_csv_and_load_sqlite3()
+        
+    option = input("\n")
+    
+    if(option == "1"):
+        FileToDatabase.read_csv_and_load_sqlite3()
+    
+
+    
+    
