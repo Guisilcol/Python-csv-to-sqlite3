370427adc0d4f0e5bd5420a76656da06709fdb92
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2021-01-17 23:19:03 -0300

    Update Main.py
    - Funções sendo chamadas no menu mas não implementadas

--- src/Main.py
+++ src/Main.py
@@ -12,9 +12,12 @@ if __name__ == "__main__":
     
     welcome_message = \
         "       ********** CSV to SQLITE3 ********** \n\n" \
-        "1 - Exportar dados de arquivos para banco de dados \n" \
+        "1 - Exportar dados de arquivos para banco de dados SQLITE3 \n" \
         "2 - Transformar arquivos .csv em arquivos posicionais \n" \
         "3 - Transformar arquivos posicionais em arquivos .csv \n" \
+        "4 - Exportar os arquivos em tabelas SQLSERVER - NÃO IMPLEMENTADO\n" \
+        "5 - Exportar os arquivos em tabelas ORACLE - NÃO IMPLEMENTADO\n" \
+        "6 - Exportar os arquivos em tabelas MYSQL - NÃO IMPLEMENTADO\n" \
         "Informe qualquer outra coisa para sair "
 
     print(welcome_message)
@@ -23,10 +26,18 @@ if __name__ == "__main__":
     
     if(option == "1"):
         FileToDatabase.read_file_and_load_sqlite3()
-    if(option == "2"):
+    elif(option == "2"):
         FileToFile.read_csv_and_load_fwf()
-    if(option == "3"):
+    elif(option == "3"):
         FileToFile.read_fwf_and_load_csv()
+    elif(option == "4"):
+        FileToDatabase.read_file_and_load_sqlserver()
+    elif(option == "5"):
+        FileToDatabase.read_file_and_load_oracle()
+    elif(option == "6"):
+        FileToDatabase.read_file_and_load_mysql()
+    else:
+        print("> Erro: A opção informada é inválida.")
     
     input("> Aperte Enter para continuar...")
 
