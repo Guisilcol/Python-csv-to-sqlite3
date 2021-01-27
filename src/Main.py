40e83593586472787113ac0f122556dd878ae4c5
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2021-01-27 12:55:01 -0300

    Adicionado processo de criação de pastas e avisos

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@ import controller.FileToDatabase as FileToDatabase
 import controller.FileToFile as FileToFile
 import warnings
 import Config
+import modules.File as File
 
 if __name__ == "__main__":    
     config = Config.get_config_dict()
@@ -10,8 +11,14 @@ if __name__ == "__main__":
     if not IS_IN_DEVELOPMENT:
         warnings.filterwarnings('ignore')           
     
+    if not File.check_if_the_folders_are_created(config['diretorios']):
+        print("\n")
+        print("> As pastas necessárias não foram criadas. O programa fará a criação de todas")
+        File.create_folders(config['diretorios'])
+        print("> As pastas foram criadas com sucesso")
+    
     welcome_message = \
-        "       ********** CSV to SQLITE3 ********** \n\n" \
+        "\n\n       ********** CSV to SQLITE3 ********** \n\n" \
         "1 - Exportar dados de arquivos para banco de dados SQLITE3 \n" \
         "2 - Transformar arquivos .csv em arquivos posicionais \n" \
         "3 - Transformar arquivos posicionais em arquivos .csv \n" \
@@ -23,7 +30,7 @@ if __name__ == "__main__":
     print(welcome_message)
         
     option = input("Informe a opção: ")
-    
+    print("\n")
     if(option == "1"):
         FileToDatabase.read_file_and_load_sqlite3()
     elif(option == "2"):
@@ -32,10 +39,13 @@ if __name__ == "__main__":
         FileToFile.read_fwf_and_load_csv()
     elif(option == "4"):
         FileToDatabase.read_file_and_load_sqlserver()
+        print("> AVISO: Funcionalidade não implementada ")
     elif(option == "5"):
         FileToDatabase.read_file_and_load_oracle()
+        print("> AVISO: Funcionalidade não implementada ")
     elif(option == "6"):
         FileToDatabase.read_file_and_load_mysql()
+        print("> AVISO: Funcionalidade não implementada ")
     else:
         print("> Erro: A opção informada é inválida.")
     
